ac9ef3eaa0edec35bf8539a9d4d14662a0582e7c
Heimen Stoffels <vistausss@outlook.com>
2020-05-30 17:07:01 +0200

    Added Dutch translation

---
+++ translations/app_nl.ts
@@ -0,0 +1,182 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>DatetimePlugin</name>
+    <message>
+        <location filename="../dde-istate-datetime/datetimeplugin.cpp" line="51"/>
+        <source>DDE Istate Datetime</source>
+        <translation>DDE Istate Datetime</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-datetime/datetimeplugin.cpp" line="156"/>
+        <source>12 Hour Time</source>
+        <translation>12-uursklok</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-datetime/datetimeplugin.cpp" line="158"/>
+        <source>24 Hour Time</source>
+        <translation>24-uursklok</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-datetime/datetimeplugin.cpp" line="164"/>
+        <source>Time Settings</source>
+        <translation>Tijdinstellingen</translation>
+    </message>
+</context>
+<context>
+    <name>IstateNetworkWidget</name>
+    <message>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="26"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="571"/>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="243"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="571"/>
+        <source>Form</source>
+        <translation>Overzicht</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="47"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="572"/>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="244"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="572"/>
+        <source>NETWORK</source>
+        <translation>NETWERK</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="167"/>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="329"/>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="723"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="575"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="585"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="612"/>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="252"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="575"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="585"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="612"/>
+        <source>↑</source>
+        <translation>↑</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="174"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="576"/>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="253"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="576"/>
+        <source>0.50 KB/s</source>
+        <translation>0.50 KB/s</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="184"/>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="265"/>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="769"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="577"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="581"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="616"/>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="254"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="577"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="581"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="616"/>
+        <source>↓</source>
+        <translation>↓</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="191"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="578"/>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="255"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="578"/>
+        <source>3.00KB/s</source>
+        <translation>3.00KB/s</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="211"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="579"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="579"/>
+        <source>STATISTICS</source>
+        <translation>STATISTIEKEN</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="587"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="603"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="603"/>
+        <source>ipv4</source>
+        <translation>ipv4</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.ui" line="609"/>
+        <location filename="../build/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="604"/>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="257"/>
+        <location filename="../cmake-build-debug/dde-istate-netspeed/dde_istate_netspeed_autogen/include/ui_istatenetworkwidget.h" line="604"/>
+        <source>PROCESSES</source>
+        <translation>PROCESSEN</translation>
+    </message>
+    <message>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="245"/>
+        <source>Public Ip</source>
+        <translation>Openbaar ip-adres</translation>
+    </message>
+    <message>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="246"/>
+        <source>78.13.198.130</source>
+        <translation>78.13.198.130</translation>
+    </message>
+    <message>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="247"/>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="248"/>
+        <source>AAA</source>
+        <translation>AAA</translation>
+    </message>
+    <message>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="249"/>
+        <source>Wi-Fi</source>
+        <translation>Wi-Fi</translation>
+    </message>
+    <message>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="250"/>
+        <source>Connected</source>
+        <translation>Verbonden</translation>
+    </message>
+    <message>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="251"/>
+        <source>192.168.0.9</source>
+        <translation>192.168.0.9</translation>
+    </message>
+    <message>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_istatenetworkwidget.h" line="256"/>
+        <source>NOT CONNECTED</source>
+        <translation>NIET VERBONDEN</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.cpp" line="29"/>
+        <source>Peak↑</source>
+        <translation>Piek↑</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.cpp" line="33"/>
+        <source>Peak↓</source>
+        <translation>Piek↓</translation>
+    </message>
+    <message>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.cpp" line="122"/>
+        <location filename="../dde-istate-widgets/istatenetworkwidget.cpp" line="123"/>
+        <source>Peak</source>
+        <translation>Piek</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../dde-istate-widgets/mainwindow.ui" line="13"/>
+        <location filename="../build-dde-istate-widgets-default-Debug/ui_mainwindow.h" line="55"/>
+        <source>MainWindow</source>
+        <translation>Hoofdvenster</translation>
+    </message>
+</context>
+<context>
+    <name>Process.Table</name>
+    <message>
+        <location filename="../utils/stats_collector.cpp" line="630"/>
+        <location filename="../utils/stats_collector.cpp" line="639"/>
+        <source>Tray</source>
+        <translation>Systeemvak</translation>
+    </message>
+</context>
+</TS>
